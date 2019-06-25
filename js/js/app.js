76afbdc3fc6567a2d90f192d3dfa75a8887863c0
manila <manilagrg@gmail.com>
2019-06-25 23:41:12 +0545

    added string alert and promt

---
+++ js/js/app.js
@@ -0,0 +1,61 @@
+///levels of log message
+///information message
+///warning message 
+//error message
+
+//variables
+var someNumber = 10;
+
+var someText ="Hellow World";
+
+console.log(someText);
+
+console.log("old somenum" , someNumber);
+
+var someNumber = 100;
+
+console.log(someNumber);
+
+console.log(20);
+
+console.log(30);
+
+console.log ("This is a string that will show up in console")
+
+var math = 50;
+var nepali = 50;
+var computer = 80;
+
+var totalObatinMarks = math + nepali + computer;
+
+var totalMarks = 300;
+
+console.log (totalObatinMarks / totalMarks * 100);
+
+console.error ("this will show error message")
+console.warn ("this will show warning message")
+
+console.log (10 * 2);
+console.log (25 % 2);
+console.log (36 + 90 - 20 - 10);
+
+
+///=============================================================================================
+
+var firstName = "Shiva"
+var middleName = "Ram"
+var lastName    = "Gurung"
+
+var fullName = firstName +" " + middleName + " "+ lastName 
+
+console. log (fullName.length);
+
+console. log(fullName.toUpperCase());
+
+console. log(lastName.toLowerCase());
+
+//alert(Hellow World)
+
+var question = prompt("Enter your Name");
+
+
