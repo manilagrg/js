ccee411cee65fb63462d197b1101ae0baef082c6
manila <manilagrg@gmail.com>
2019-06-27 14:46:25 +0545

    add inan

--- js/js/conditionals.js
+++ js/js/conditionals.js
@@ -24,8 +24,35 @@ if (question.length > message.length){
     console.log("largest answer "+question);
     
 } else {console.log("largest answer "+message);
+
 }
 
+//====================================================
+var firstString = prompt("Enter a number");
+var secondString = prompt("Enter another number");
+
+console.log ("the types of varibles before the conversition", typeof firstString)
+
+var firstNum = parseInt(firstString);
+var secondNum = parseInt(secondString);
+
+console.log ("the types of variables after the conversition", typeof firstNum)
+
+if(firstNum > secondNum)
+{
+    console.log("The first num is greater with the value of ", firstNum)
+} else{
+    console.log("The second num is greater with the value of ", secondNum)
+}
+
+if(isNaN (firstNum)){
+    console.error ("the number you enter is not valid")
+
+}
+
+if(isNaN (secondNum)){
+    console.error ("the second number you enter is not valid")
+}
 
 //ask 2 questions 
 // get the size of each question
