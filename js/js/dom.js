85a94425b197078454e643b47cd41db837e556d5
manila <manilagrg@gmail.com>
2019-07-25 22:44:11 +0545

    Added bg change on interval and clearinterval

--- js/js/dom.js
+++ js/js/dom.js
@@ -1,6 +1,6 @@
 var continer = document.getElementsByClassName("continer")
 
-var header = document.getElementById("header")
+//var header = document.getElementById("header")
 
 var firstContiner = document.querySelector(".continer")
 
@@ -8,10 +8,12 @@ var parapraph = document.querySelectorAll('p');
 
 var allDivs = document.querySelectorAll('div')
 
-//var allHeadings = document.getElementsByTagName("h1")
-var firstHeader = document.querySelector("#header h1")
+var button = document.querySelectorAll("button")
 
-var herf = document.querySelector("a")
+//var allHeadings = document.getElementsByTagName("h1")
+var firstHeader = document.querySelector("header")
+
+//var herf = document.querySelector("a")
 
 // creat the three paragarph and change the color accourdly 
 
@@ -38,19 +40,125 @@ function changeBackground(){
 firstContiner.style = "background-color: red"
 
 }
+button[0].onclick = changeBackground
 
 function myFunction(){
     //document.getElementById('header').style = "background-color: black; color: white"
     header.style = "background-color: black; color: white"
 }
-
+//////////==============================
+//The following example are same only way of method is different 
+/*
 function changeColor(){
     //document.querySelectorAll('p')[2].style = "background-color: red"
-    parapraph[2].style = "background-color: black; color: white"
+    parapraph[2].style = "background-color: blue; color: white"
 }
 
-var body = document.getElementsByTagName("body")
-function bodyColor(){
-    //document.getElementsByTagName("body")[0].style = "background-color: red"
-    body[0].style = "background-color: green"
+parapraph[2].onclick = changeColor
+*/
+button[2].onclick = function(){
+    parapraph[2].style = "background-color: blue; color: white"
 }
+
+//==========================
+
+/*
+function bodyColor(){
+    document.body.style = "background-color: green";
+    
+}
+*/
+
+
+//window.body[0].onclick = bodyColor
+
+//create an page with nav bar, picture and to change the background onclick
+//to redrict another page
+//docment.location.assign(url)
+//we can use onclick directly on js
+firstHeader.onclick = myFunction
+/*
+allDivs[1].onclick = function(){
+    console.log("click on header")
+}
+*/
+function changeInfo(){
+    allDivs[1].innerText = "Get some information about js and practice it"
+}
+allDivs[1].onclick = changeInfo
+/*
+firstHeader.onclick = function(event){
+    //console.log(event.target)
+    firstHeader.classList.add("header")
+}
+*/
+/*
+setInterval(function(count){
+    //for(var i = 0; i < count; i++)
+    console.log("Hello World")
+    
+
+}, 5000)
+*/
+
+//setTimeout(function(){alert ("Hello There!"), 2000})
+//interval with loop
+
+function changeHeader() {
+    var heading = document.querySelector('header h1')
+    heading.innerText = "Another Header"
+}
+document.querySelector('header h1').onclick = changeHeader
+
+/*
+document.oncontextmenu = function(event){
+    event.preventDefault();
+    //document.body.style = "background-color: blue"
+}
+*/
+
+function navigateTo(event) {
+    console.log(event)
+    event.preventDefault();
+    event.target.innerText = "Clicked"
+    let href = event.target.getAttribute('href');
+    //Set timeout fires the function after the duration
+    setTimeout(function () {
+        //Redirect to anothre page
+        document.location.assign(href)
+    }, 5000)
+}
+
+document.getElementById('navigate').onclick = navigateTo
+document.getElementById('another').onclick = navigateTo
+
+//Math.round to convert decimal on number
+function randomnumber(max = 255){
+    return Math.round(Math.random() * max)
+}
+
+//document.body.style = "background-color: rgb(randomnumber + "," + randomnumber + "," + randomnumber )"
+
+// document.body.style = "background-color: rgb("+ randomnumber() + "," + randomnumber() + "," + randomnumber() +")" 
+
+
+//document.body.style = `background-color: rgb(${randomnumber()},${randomnumber()},${randomnumber()})`
+//`` back tick
+function changbodybg(){
+    //document.body.style = "background-color: rgb("+ randomnumber() + "," + randomnumber() + "," + randomnumber() +")" 
+    document.body.style = `background-color: rgb(${randomnumber()},${randomnumber()},${randomnumber()})`
+}
+
+var stopInterval = setInterval(changbodybg, 1000)
+
+
+button[3].onclick = function(){
+    clearInterval(stopInterval)
+}
+
+setTimeout(function(){
+    clearInterval(stopInterval)
+},10000)
+
+
+
